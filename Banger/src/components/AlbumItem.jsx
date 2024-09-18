eae4f14a269c4344ca40d348e5c3e34a02773100
subbu <subramanyaprabhu1001@gmail.com>
2024-09-17 16:47:04 -0800

    first-commit

--- Banger/src/components/AlbumItem.jsx
+++ Banger/src/components/AlbumItem.jsx
@@ -5,8 +5,8 @@ const AlbumItem = ({ image, name, desc, id }) => {
   const navigate = useNavigate();
 
   return (
-    <div onClick={() => navigate(`/album/${id}`)} className='min-w-[120px] p-2 rounded cursor-pointer hover:bg-[#ffffff26]'>
-      <img className='rounded w-24 h-24' src={image} alt={name} />
+    <div onClick={() => navigate(`/album/${id}`)} className='min-w-[140px] p-2 rounded cursor-pointer hover:bg-[#ffffff26]'>
+      <img className='rounded w-28] h-28' src={image} alt={name} />
       <p className='font-bold mt-2 text-sm'>{name}</p>
       <p className='text-slate-200 text-xs'>{desc}</p>
     </div>
